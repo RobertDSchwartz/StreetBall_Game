638282d1190348c1de57a418062005ea197d3c64
RobertDSchwartz <schwartzr2016@gmail.com>
2022-02-14 10:50:33 -0600

    after class on monday

--- src/StreetBall_Player.java
+++ src/StreetBall_Player.java
@@ -2,26 +2,29 @@ import java.text.NumberFormat;
 
 public class StreetBall_Player {
 	private String name;
+	private int number;
 	private int onePointers;
 	private int twoPointers;
 	private int threePointers;
 	private int fouls;
 	private int attempts;
-	private int madeattempts;
 	private double shotPercentage;
 	
 	
 	public StreetBall_Player() {
 		this.name = null;
+		this.number = 0;
 		this.onePointers = 0;
 		this.twoPointers = 0;
 		this.threePointers = 0;
 		this.fouls = 0;
 		this.attempts = 0;
-		this.madeattempts = 0;
 		this.shotPercentage = 0;
 	}
 
+	public StreetBall_Player(String name) {
+		this.name = null;
+	}
 
 	public String getName() {
 		return name;
@@ -33,35 +36,29 @@ public class StreetBall_Player {
 	}
 
 
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void shotAttempt(int shotType, boolean made) {
+		
+	}
+
 	public int getOnePointers() {
 		return onePointers;
 	}
-
-
-	public void setOnePointers(int onePointers) {
-		this.onePointers = onePointers;
-	}
-
 
 	public int getTwoPointers() {
 		return twoPointers;
 	}
 
-
-	public void setTwoPointers(int twoPointers) {
-		this.twoPointers = twoPointers;
-	}
-
-
 	public int getThreePointers() {
 		return threePointers;
 	}
-
-
-	public void setThreePointers(int threePointers) {
-		this.threePointers = threePointers;
-	}
-
 
 	public int getFouls() {
 		return fouls;
@@ -82,17 +79,7 @@ public class StreetBall_Player {
 		this.attempts = attempts;
 	}
 
-
-	public int getMadeattempts() {
-		return madeattempts;
-	}
-
-
-	public void setMadeattempts(int madeattempts) {
-		this.madeattempts = madeattempts;
-	}
-
-
+	
 	public double getShotPercentage() {
 		return shotPercentage;
 	}
